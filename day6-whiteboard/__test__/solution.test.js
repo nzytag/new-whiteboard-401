8a6a0c30fa64fd1703737b9d615b037574446a65
Hector <norzagaray@me.com>
2018-01-22 19:21:39 -0800

    added solution and test

--- day6-whiteboard/__test__/solution.test.js
+++ day6-whiteboard/__test__/solution.test.js
@@ -1,19 +1,19 @@
 'use strict';
-const solution = require('../lib/solution.js');
+const recurs = require('../lib/solution.js');
 
-describe('Solutions', function () {
-  describe('#loop', () => {
+describe('Recursive Module', function () {
+  describe('#Recursive Loop', () => {
     it('this should return undefined when the count is equal to 0', () => {
       let count = 19;
-      let callback = solution.loop();
-      expect(solution.loop(count, callback)).toBe(undefined);
+      let callback = recurs.loop();
+      expect(recurs.loop(count, callback)).toBe(undefined);
     });
     it('should return null if less than 2 arguments are passed in', () => {
-      expect(solution.loop()).toBeNull();
+      expect(recurs.loop()).toBeNull();
     });
     it('returns null if other than an integer is passed', () => {
-      let callback = solution.loop();
-      expect(solution.loop('a', callback)).toBeNull();
+      let callback = recurs.loop();
+      expect(recurs.loop('a', callback)).toBeNull();
     });
   });
 });
--- day6-whiteboard/lib/solution.js
+++ day6-whiteboard/lib/solution.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const solution = module.exports = {};
+const recurs = module.exports = {};
 //first we take a function that takes in 2 arguments in line 5
-solution.loop = function(count, callback) {
+recurs.loop = function(count, callback) {
   //then we tell it that if the argument is equal to 0, to be undefined
   if(count === 0) return undefined;
 
@@ -10,11 +10,11 @@ solution.loop = function(count, callback) {
   if (!count) return null;
   //this is for when the argument passed is not a number, return null
   if (typeof count !== 'number' || count < 0) return null;
-  
-  //but if the count is higher than 0, decrement to get to 0
+
+  //but if the count is more than 0, decrement to get to 0
   count --;
   //this is a recursive callback to pass in the decrement values
-  solution.loop(count, callback);
+  recurs.loop(count, callback);
 };
 
 
